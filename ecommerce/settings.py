d5fce0499e18b9bd65273456708fe44072244cf8
SripathiAvinashKumar <avinashsripathi4@gmail.com>
2024-09-22 01:12:41 +0530

    changed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -56,7 +56,7 @@ NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    # 'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     # 'django.template.context_processors.request',
     'django.middleware.common.CommonMiddleware',
@@ -144,7 +144,7 @@ STATICFILES_DIRS =[
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_DIR = [BASE_DIR/'static']
 
